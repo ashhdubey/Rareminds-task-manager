094fef2f08269b3c2173a567d2d342c3a5669e9e
Ashish Kumar Dubey <ashishdubey.work@gmail.com>
2025-12-16 16:31:40 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rareminds-backend-29x3.onrender.com',
+  baseURL: 'https://rareminds-backend-29x3.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
