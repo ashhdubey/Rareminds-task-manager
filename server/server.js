316a66e6cde67f7f1cd7fb8fd5ff0b9d5c783d23
Ashish Kumar Dubey <ashishdubey.work@gmail.com>
2025-12-16 15:16:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app); // Create HTTP server
 // Initialize Socket.io with CORS to allow client connection
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:3000"], // Adjust if your frontend port differs
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://raremindstaskmanager.netlify.app"], // Adjust if your frontend port differs
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 });
